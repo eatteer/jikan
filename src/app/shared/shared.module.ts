cbb6635d859cf9df8a74ddb2b904c1ff9d4ce393
Practicante Programacion TI <practicante.ti2@austinreed.com.co>
2023-03-14 11:35:12 -0500

    Implement detail page and other things

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SafeUrlPipe } from './pipes/safe-url.pipe';
+import { AppbarComponent } from './components/appbar/appbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [SafeUrlPipe],
-  imports: [CommonModule],
-  exports: [SafeUrlPipe],
+  declarations: [SafeUrlPipe, AppbarComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [SafeUrlPipe, AppbarComponent],
 })
 export class SharedModule {}
